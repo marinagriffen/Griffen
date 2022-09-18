fe081ff4ec1f8d68f916088c4c85f6548f48b17a
Griffen <marina.griffen14@gmail.com>
2022-09-18 18:17:01 +0300

    rename

---
+++ cypress/e2e/lessonPractice.cy.js
@@ -0,0 +1,21 @@
+
+describe ('Sign up page form', () => {
+
+    before (() => {
+      cy.visit('https://www.beaxy.com/register/')
+  
+    });
+  
+    it ('Form', () => {
+        cy.get('[name="email"]').type('abc@gmail.com');
+        cy.get('#_pass').type('Kd1234#5678');
+        cy.get('.selected-country-arrow>svg').click(); //svg 
+        cy.get('div[class="custom-select-wrap"]').click();
+        cy.get('[name="phoneNumber"]').type('543543545')
+        cy.get('#popUp-register-promo').type('test');
+        cy.get ('[data-type="castomCheckbox"]').click();
+        cy.get('form[data-form="register"] button[type="submit"]').click()
+ 
+    });
+   
+  });
